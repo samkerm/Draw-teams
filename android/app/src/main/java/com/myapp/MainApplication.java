30b48b0f51ec9b043e9839c55e334f92f7131dc3
Claire Peng <clairepeng@Claires-MacBook-Pro.local>
2018-08-14 17:03:13 -0700

    Added qr code scanner

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -3,11 +3,6 @@ package com.myapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -15,8 +10,6 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
-
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,13 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage(),
-            new RNCameraPackage(),
-            new RNFetchBlobPackage(),
-            new ImagePickerPackage(),
-            new VectorIconsPackage(),
-          new ReactNativePushNotificationPackage()
+          new MainReactPackage()
       );
     }
 
